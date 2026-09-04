d5b6878420fb804a5fd45af6fa4f7b4d7d947650
RizqiAulia23 <EMAIL_GITHUB_KAMU>
2026-09-04 16:45:48 +0700

    update admin

---
+++ supabese.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://ivgnzcwvtkayfuuimhzo.supabase.co";
+const SUPABASE_KEY = "sb_publishable_mJsG7F6zw4RWljeQeegu6Q_cwSlXD2S";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
